b12588726e71dca99b3e6d06a64867ba118d1a35
qljtm <qljtmalang@tip.edu.ph>
2026-02-28 20:47:07 +0800

    Delete src directory

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      Made with <span>♥</span> &amp; <span>☕</span> by Coffeecient &copy; {new Date().getFullYear()}
-    </footer>
-  );
-}
